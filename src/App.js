1d97029f0098b717f2f700b2abc80066b1fb25ac
Bruno <b.hpereira97@gmail.com>
2023-12-09 11:09:06 -0300

    home page ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Projects from "./components/pages/Projects";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
 
+import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
   );
 }
 
